8544a1904ac5531b960d2673cc2a0824a8eee4c1
jackgray26 <jgray26@dcs.bbk.ac.uk>
2020-09-21 16:52:52 +0100

    Started random number generator

--- PracticeGame/PracticeGame.cpp
+++ PracticeGame/PracticeGame.cpp
@@ -13,5 +13,13 @@ using namespace std;
 
 int main()
 {
+	//random number generator using system time, result between 0-99
+	srand(time(NULL));
+	unsigned int numberToGuess = rand() % 100;
+	cout << "Guess a number between 0 and 99: " << endl;
 
+	unsigned int playersNumber{};
+	cin >> playersNumber;
+
+	cout << "You have guessed: " << playersNumber << " "
 }
