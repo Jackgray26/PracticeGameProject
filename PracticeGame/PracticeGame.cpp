e6ab5d69a2b1b72a550584fbe484cbf99ae92397
jackgray26 <jgray26@dcs.bbk.ac.uk>
2020-09-21 16:37:19 +0100

    Added Main with Header files

---
+++ PracticeGame/PracticeGame.cpp
@@ -0,0 +1,17 @@
+//C++ guessing game main file
+
+#pragma region Header_Files
+
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#pragma endregion 
+
+using namespace std;
+
+int main()
+{
+
+}
