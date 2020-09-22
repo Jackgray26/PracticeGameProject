e66ea5229bdcd65de8914f8862722687526fd9ee
jackgray26 <jgray26@dcs.bbk.ac.uk>
2020-09-22 22:44:19 +0100

    Finished Name entry system

--- PracticeGame/PracticeGame.cpp
+++ PracticeGame/PracticeGame.cpp
@@ -17,6 +17,12 @@ int main()
 	cout << "Welcome to your text adventure!" << endl << endl;
 	cout << "Please enter your Name" << endl << endl;
 
+	//Player Name can only be up to 1024 characters long, stored as playerName
+	char playerName[1024];
+	cin >> playerName;
 
+	cout << endl << "Hello " << playerName << endl;
+
+	return 0;
 
 }
